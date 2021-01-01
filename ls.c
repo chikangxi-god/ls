c4f59ce92cf8d99568ea4597ce4b0562beac7cd7
chikangxi <chikangxi@hotmail.com>
2021-01-01 16:43:10 +0800

    Add error reporting.

--- ls.c
+++ ls.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <errno.h>
 
 int
 main (void)
 {
   DIR *dirstream = opendir (".");
+  if (dirstream == NULL)
+    {
+      perror(NULL);
+      return -1;
+    }
   struct dirent *entry;
   while (entry = readdir (dirstream))
     {
